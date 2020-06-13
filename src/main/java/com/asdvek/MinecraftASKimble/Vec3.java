92d5f3b0cec4cb8d26180f2904a0ae054a2d45d1
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2020-06-14 00:39:57 +0300

    Fix Vec3 setters copy paste bug.

--- src/main/java/com/asdvek/MinecraftASKimble/Vec3.java
+++ src/main/java/com/asdvek/MinecraftASKimble/Vec3.java
@@ -18,8 +18,8 @@ public class Vec3 {
 
     // setters
     public void setX(double x) { this.x = x; }
-    public void setY(double x) { this.x = x; }
-    public void setZ(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setZ(double z) { this.z = z; }
     public void set(double x, double y, double z) { this.x = x; this.y = y; this.z = z; }
 
     /* constructors */
